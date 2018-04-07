d2fef274f41ef1cf73ebad0b6a289a17459d263b
Declimator <league@WTS-iMac-11.attlocal.net>
2018-04-06 18:25:03 -0700

    MyFirstSwingGUI, DuelingButtons, DrumKit

--- src/DrumKit.java
+++ src/DrumKit.java
@@ -3,7 +3,6 @@
  *    Level 1
  */
 
-
 import java.applet.AudioClip;
 import java.awt.GridLayout;
 import java.awt.event.MouseAdapter;
@@ -20,59 +19,73 @@ import javax.swing.JPanel;
 
 public class DrumKit extends MouseAdapter {
 
-    JLabel drumLabelWithImage;
+	JLabel drumLabelWithImage;
+	JLabel drumLabelWithImagetwo;
+	public static void main(String[] args) throws Exception {
+		new DrumKit().getGoing();
+	}
 
-    public static void main(String[] args) throws Exception {
-   	 new DrumKit().getGoing();
-    }
+	private void getGoing() throws MalformedURLException {
+		// 1. Make a JFrame variable and initialize it using "new JFrame()"
+		JFrame frame = new JFrame();
+		// 2. Make the frame visible
+		frame.setVisible(true);
+		// 3. Set the size of the frame
+		frame.setSize(500, 500);
+		// 4. Set the title of the frame
+		frame.setTitle("DrumKit");
+		// 5. Make a JPanel and initialize it.
+		JPanel panel = new JPanel();
+		// 6. Add the panel to the frame. (The panel is invisible.)
+		frame.add(panel);
+		// 7. Download an image of a drum from the Internet. Drop it into your Eclipse
+		// project under "default package".
+		// 8. Put the name of your image in a String variable.
+		String image = "snare.jpg";
+		String imagetwo = "cymbal.jpg";
+		// 9. Edit the next line to use your String variable
+		// drumLabelWithImage = createLabelImage(drumImageString);
+		drumLabelWithImage = createLabelImage(image);
+		drumLabelWithImagetwo = createLabelImage(imagetwo);
+		// 10. Add the image to the panel
+		panel.add(drumLabelWithImage);
+		panel.add(drumLabelWithImagetwo);
+		// 11. Set the layout of the panel to "new GridLayout()"
+		panel.setLayout(new GridLayout());
+		// 12. call the pack() method on the frame
+		frame.pack();
+		// 13. add a mouse listener to drumLabelWithImage.
+		drumLabelWithImage.addMouseListener(this);
+		drumLabelWithImagetwo.addMouseListener(this);
+	}
 
-    private void getGoing() throws MalformedURLException {
-   	 // 1. Make a JFrame variable and initialize it using "new JFrame()"
-   	 
-   	 // 2. Make the frame visible
-   	 
-   	 // 3. Set the size of the frame
-   	 
-   	 // 4. Set the title of the frame
-   	 
-   	 // 5. Make a JPanel and initialize it.
-   	 
-   	 // 6. Add the panel to the frame. (The panel is invisible.)
-    
-   	 // 7. Download an image of a drum from the Internet. Drop it into your Eclipse project under "default package".
-   	 // 8. Put the name of your image in a String variable.
-    
-   	 // 9. Edit the next line to use your String variable
-// drumLabelWithImage = createLabelImage(drumImageString);
-   	 
-   	 // 10. Add the image to the panel
-    
-   	 // 11. Set the layout of the panel to "new GridLayout()"
-    
-   	 // 12. call the pack() method on the frame
-   	 
-   	 // 13. add a mouse listener to drumLabelWithImage.
+	public void mouseClicked(MouseEvent e) {
+		// 14. When the mouse is clicked, print "mouse clicked"
 
-    }
+		System.out.println("mouse clicked");
 
-    public void mouseClicked(MouseEvent e) {
-   	 // 14. When the mouse is clicked, print "mouse clicked"
+		JLabel drumClicked = (JLabel) e.getSource();
 
-   	 JLabel drumClicked = (JLabel) e.getSource();
-   	 // 15. Download a drum sound and drop it into your "default package". You can find it on freesound.org. To download it, log in as leagueofamazing/code4life.
-   	 // 16. If they clicked on the drumImage...
+		// 15. Download a drum sound and drop it into your "default package". You can
+		// find it on freesound.org. To download it, log in as
+		// leagueofamazing/code4life.
+		// 16. If they clicked on the drumImage...
+		if (drumClicked==drumLabelWithImage) {
+			// 17. ...use the playSound method to play a drum sound.
+			playSound("drum.wav");
+		}
+		if(drumClicked==drumLabelWithImagetwo) {
+			playSound("cymbal.wav");
+		}
 
-   		 // 17. ...use the playSound method to play a drum sound.
 
-   	 // 18. Add more images to make a drumkit. Remember to add a mouse listener to each one.
-    }
+		// 18. Add more images to make a drumkit. Remember to add a mouse listener to
+		// each one.
+	}
 
-    private JLabel createLabelImage(String fileName)
-			throws MalformedURLException
-	{
+	private JLabel createLabelImage(String fileName) throws MalformedURLException {
 		URL imageURL = getClass().getResource(fileName);
-		if (imageURL == null)
-		{
+		if (imageURL == null) {
 			System.err.println("Could not find image " + fileName);
 			return new JLabel();
 		}
@@ -81,13 +94,9 @@ public class DrumKit extends MouseAdapter {
 		return imageLabel;
 	}
 
-
-    private void playSound(String fileName) {
-   	 AudioClip sound = JApplet.newAudioClip(getClass().getResource(fileName));
-   	 sound.play();
-    }
+	private void playSound(String fileName) {
+		AudioClip sound = JApplet.newAudioClip(getClass().getResource(fileName));
+		sound.play();
+	}
 
 }
-
-
-
--- src/MyFirstSwingGUI.java
+++ src/MyFirstSwingGUI.java
@@ -15,31 +15,32 @@ import javax.swing.JPanel;
 public class MyFirstSwingGUI {
 	public static void main(String[] args) {
 		//1. Create and initialize an object of the JFrame class
+JFrame myFrame = new JFrame();
 
 		//2. Set your JFrame object to be visible
-
+myFrame.setVisible(true);
 		//3. Run your program. Do you see your window? It's probably very small.
 		
 		//4. Set the default close operation to JFrame.EXIT_ON_CLOSE
-
+myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		//5. Create and initialize an object of the JPanel class
-
+JPanel myPanel = new JPanel();
 		//6. Create and initialize an object of the JLabel class
-
+JLabel myLabel = new JLabel();
 		//7. Set the text of the JLabel to a lovely greeting.
-
+myLabel.setText("hi");
 		//8. Add the JPanel object to the JFrame
-
+myFrame.add(myPanel);
 		//9. Add the JLabel object to the JPanel
-
+myPanel.add(myLabel);
 		//10. Pack your JFrame.
-
+myFrame.pack();
 		//11. Run your program again. Do you see your message.
 		
 		//12. Use the loadImage method to set the icon of the JLabel object.  
-
+myLabel.setIcon(loadImage());
 		//13. Re-pack the JFrame object.
-		
+		myFrame.pack();
 		//14. Run the program one more time. Do you see the image?
 	}
 	
